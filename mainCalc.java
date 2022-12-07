f07c643b57ba3f3bd0efcfc4531ed0bb72716318
Vijay112003 <120007347+Vijay112003@users.noreply.github.com>
2022-12-07 13:49:46 +0530

    After buttons declaration

--- mainCalc.java
+++ mainCalc.java
@@ -1,2 +1,20 @@
 import java.awt.*;
 import java.awt.event.*;
+class DemoButton extends Frame implements ActionListener
+{
+	TextField t1,t2,t3;
+	Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt,8,bt9,bt0,btsm,btsb,btml,btdi,bteq;
+	Label l1,l2,l3;
+	DemoButton()
+	{
+		
+	}
+
+	public static void main(String args[]){
+		DemoButton D1 = new DemoButton();
+	}
+
+
+
+
+}
