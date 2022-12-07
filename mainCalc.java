df1e93c66b3276a47ac80c747b5dd02def647cea
hemasundar <spideysundar2004@gmail.com>
2022-12-07 15:41:06 +0530

    added listeners

--- mainCalc.java
+++ mainCalc.java
@@ -1,22 +1,175 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.plaf.LabelUI;
-class DemoButton extends Frame implements ActionListener
+class DemoButton extends Frame implements ActionListener,MouseListener,MouseMotionListener
 {
-	TextField t1,t2,t3;
-	Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt0,btsm,btsb,btml,btdi,bteq;
+	TextField txans;
+	Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt0,btsm,btsb,btml,btdi,bteq,btdt,btac,pothu;
 	Label l1;
 	DemoButton()
 	{
-				
+		super("HTV Calcutor 2.0");
+
+		txans=new TextField("Enter numbers");
+		txans.setBounds(10,50,215,55);
+		add(txans);
+		txans.addActionListener(this);
+		txans.addMouseListener(this);
+		txans.addMouseMotionListener(this);
+
+		bt1=new Button("1");
+		bt1.setBounds(10,110,50,50);
+		add(bt1);
+		bt1.addActionListener(this);
+		bt1.addMouseListener(this);
+		bt1.addMouseMotionListener(this);
+
+		bt2=new Button("2");
+		bt2.setBounds(65,110,50,50);
+		add(bt2);
+		bt2.addActionListener(this);
+		bt2.addMouseListener(this);
+		bt2.addMouseMotionListener(this);
+
+		bt3=new Button("3");
+		bt3.setBounds(120,110,50,50);
+		add(bt3);
+		bt3.addActionListener(this);
+		bt3.addMouseListener(this);
+		bt3.addMouseMotionListener(this);
+
+		bt4=new Button("4");
+		bt4.setBounds(10,165,50,50);
+		add(bt4);
+		bt4.addActionListener(this);
+		bt4.addMouseListener(this);
+		bt4.addMouseMotionListener(this);
+
+		bt5=new Button("5");
+		bt5.setBounds(65,165,50,50);
+		add(bt5);
+		bt5.addActionListener(this);
+		bt5.addMouseListener(this);
+		bt5.addMouseMotionListener(this);
+
+		bt6=new Button("6");
+		bt6.setBounds(120,165,50,50);
+		add(bt6);
+		bt6.addActionListener(this);
+		bt6.addMouseListener(this);
+		bt6.addMouseMotionListener(this);
+
+		bt7=new Button("7");
+		bt7.setBounds(10,220,50,50);
+		add(bt7);
+		bt7.addActionListener(this);
+		bt7.addMouseListener(this);
+		bt7.addMouseMotionListener(this);
+
+		bt8=new Button("8");
+		bt8.setBounds(65,220,50,50);
+		add(bt8);
+		bt8.addActionListener(this);
+		bt8.addMouseListener(this);
+		bt8.addMouseMotionListener(this);
+
+		bt9=new Button("9");
+		bt9.setBounds(120,220,50,50);
+		add(bt9);
+		bt9.addActionListener(this);
+		bt9.addMouseListener(this);
+		bt9.addMouseMotionListener(this);
+
+		bt0=new Button("0");
+		bt0.setBounds(10,275,50,50);
+		add(bt0);
+		bt0.addActionListener(this);
+		bt0.addMouseListener(this);
+		bt0.addMouseMotionListener(this);
+
+		btsm=new Button("+");
+		btsm.setBounds(175,110,50,50);
+		add(btsm);
+		btsm.addActionListener(this);
+		btsm.addMouseListener(this);
+		btsm.addMouseMotionListener(this);
+
+		btsb=new Button("-");
+		btsb.setBounds(175,165,50,50);
+		add(btsb);
+		btsb.addActionListener(this);
+		btsb.addMouseListener(this);
+		btsb.addMouseMotionListener(this);
+
+		btml=new Button("x");
+		btml.setBounds(175,220,50,50);
+		add(btml);
+		btml.addActionListener(this);
+		btml.addMouseListener(this);
+		btml.addMouseMotionListener(this);
+
+		btdi=new Button("/");
+		btdi.setBounds(175,275,50,50);
+		add(btdi);
+		btdi.addActionListener(this);
+		btdi.addMouseListener(this);
+		btdi.addMouseMotionListener(this);
+
+		bteq=new Button("=");
+		bteq.setBounds(120,275,50,50);
+		add(bteq);
+		bteq.addActionListener(this);
+		bteq.addMouseListener(this);
+		bteq.addMouseMotionListener(this);
+
+		btac=new Button("AC");
+		btac.setBounds(10,330,50,50);
+		add(btac);
+		btac.addActionListener(this);
+		btac.addMouseListener(this);
+		btac.addMouseMotionListener(this);
+
+		btdt=new Button(".");
+		btdt.setBounds(65,275,50,50);
+		add(btdt);
+		btdt.addActionListener(this);
+		btdt.addMouseListener(this);
+		btdt.addMouseMotionListener(this);
+
+		setSize(400,400);
+		setLayout(null);
+		setVisible(true);
 	}
-	DemoLabel()
+	public void actionPerformed(ActionEvent e){}
+	public void mouseMoved(MouseEvent e){
+		var S = e.getSource();
+		
+
+	}
+	public void mouseDragged(MouseEvent e){}
+	public void mouseClicked(MouseEvent e)
 	{
-		l1 = new  Label("Calcutor 2.0");
-		l1.setBounds(2,100,50,60);
+		if(e.getSource()==txans)
+			txans.setText("");
+	}
+	public void mouseEntered(MouseEvent e)
+	{
+		//hjvgdvf
+	}
+	public void mouseExited(MouseEvent e)
+	{
+		//yusdgf
+	}
+	public void mouseReleased(MouseEvent e)
+	{
+		//yugsd
+	}
+	public void mousePressed(MouseEvent e)
+	{
+		//hyugsdyu
 	}
 	public static void main(String args[]){
-		DemoButton D1 = new DemoButton();
+		new DemoButton();
 	}
 
 
