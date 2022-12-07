916d4c0e7d94c04cab2717ceb74d418c24844bd1
hemasundar <spideysundar2004@gmail.com>
2022-12-07 14:11:43 +0530

    added Label

--- mainCalc.java
+++ mainCalc.java
@@ -1,15 +1,20 @@
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.plaf.LabelUI;
 class DemoButton extends Frame implements ActionListener
 {
 	TextField t1,t2,t3;
-	Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt,bt8,bt9,bt0,btsm,btsb,btml,btdi,bteq;
+	Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt0,btsm,btsb,btml,btdi,bteq;
 	Label l1;
 	DemoButton()
 	{
 				
 	}
-	
+	DemoLabel()
+	{
+		l1 = new  Label("Calcutor 2.0");
+		l1.setBounds(2,100,50,60);
+	}
 	public static void main(String args[]){
 		DemoButton D1 = new DemoButton();
 	}
